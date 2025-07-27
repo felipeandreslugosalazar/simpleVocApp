00362a9d9c09b83fecd2da045776a398ad55fe48
felipe andres lugo salazar <felipeandreslugo@gmail.com>
2025-07-27 23:59:42 +0200

    add service-worker.js | register service-worker

--- script.js
+++ script.js
@@ -123,5 +123,13 @@ function enableNavigation() {
 }
 
 document.addEventListener("DOMContentLoaded", () => {
+
+  if ("serviceWorker" in navigator) {
+    navigator.serviceWorker
+      .register("/service-worker.js")
+      .then(() => console.log("✅ Service Worker registrado"))
+      .catch(err => console.error("❌ Error al registrar SW:", err));
+  }
+
   start();
 });
